90e665fe9a0c716d6e26de101487f3e135a19ec1
aviraj1703 <67193881+aviraj1703@users.noreply.github.com>
2021-08-09 22:56:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const Translate_L = () => {
     let left = document.getElementById('scroll');
     left.style.transform = `translateY(${counter + 10}rem)`;
     counter = counter + 10;
+    console.log(counter);
 }
 const Translate_R = () => {
     if (counter < -20) {
@@ -14,4 +15,5 @@ const Translate_R = () => {
     let right = document.getElementById('scroll');
     right.style.transform = `translateY(${counter - 10}rem)`;
     counter = counter - 10;
+    console.log(counter);
 }
