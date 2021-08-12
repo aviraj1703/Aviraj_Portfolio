3f9830050dc7bb095845e8a78eef4a40b00b3490
aviraj1703 <67193881+aviraj1703@users.noreply.github.com>
2021-08-12 21:44:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const Translate_L = () => {
     }
     let left = document.getElementById('scroll');
     left.style.transform = `translateY(${counter + 10}rem)`;
+    console.log(counter);
     counter = counter + 10;
 }
 const Translate_R = () => {
@@ -13,5 +14,27 @@ const Translate_R = () => {
     }
     let right = document.getElementById('scroll');
     right.style.transform = `translateY(${counter - 10}rem)`;
+    console.log(counter);
     counter = counter - 10;
+}
+
+let clk = 0;
+const menubar = () =>{
+    let btn = document.getElementById('hamburger');
+    let btn1 = document.getElementsByClassName('fst')[0];
+    let btn2 = document.getElementsByClassName('lst')[0];
+
+    if(clk == 0){
+        btn.style.height = `auto`;
+        btn1.style.display = `none`;
+        btn2.style.display = `block`;
+        clk++;
+    }
+
+    else{
+        btn.style.height = `${1.8}rem`;
+        btn1.style.display = `block`;
+        btn2.style.display = `none`;
+        clk--;
+    }
 }
