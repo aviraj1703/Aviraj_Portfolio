1db82197a9e3e5ec665f80f47c0a325aeed2c673
aviraj1703 <67193881+aviraj1703@users.noreply.github.com>
2021-09-04 16:03:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
+// Skills.js
 let counter = 0
+let left_button = document.getElementsByClassName('arr_l')[0];
+left_button.style.visibility = `hidden`;
+let right_button = document.getElementsByClassName('arr_r')[0];
 const Translate_L = () => {
-    if (counter >= 0) {
-        return;
-    }
     let left = document.getElementById('scroll');
     left.style.transform = `translateY(${counter + 10}rem)`;
-    console.log(counter);
     counter = counter + 10;
+    console.log(counter);
+    if (counter >= 0) {
+        left_button.style.visibility = `hidden`;
+    }
+    else if(counter >= -20){
+        right_button.style.visibility = `visible`;
+    }
 }
 const Translate_R = () => {
-    if (counter < -20) {
-        return;
-    }
     let right = document.getElementById('scroll');
     right.style.transform = `translateY(${counter - 10}rem)`;
-    console.log(counter);
     counter = counter - 10;
+    console.log(counter);
+    if (counter < -20) {
+        right_button.style.visibility = `hidden`;
+    }
+    else if(counter < 0){
+        left_button.style.visibility = `visible`;
+    }
 }
 
+// menubar.js
 let clk = 0;
 const menubar = () =>{
     let btn = document.getElementById('hamburger');
     let btn1 = document.getElementsByClassName('fst')[0];
     let btn2 = document.getElementsByClassName('lst')[0];
-
+    let body = document.getElementsByTagName("body")[0];
     if(clk == 0){
-        btn.style.height = `auto`;
+        btn.style.height = `${100}%`;
         btn1.style.display = `none`;
         btn2.style.display = `block`;
+        body.style.overflow = `hidden`;
         clk++;
     }
 
@@ -35,23 +47,35 @@ const menubar = () =>{
         btn.style.height = `${1.8}rem`;
         btn1.style.display = `block`;
         btn2.style.display = `none`;
+        body.style.overflowY = `scroll`;
         clk--;
     }
 }
 
+// onload.js
 let cnt = 0;
-const alertt = () => {
+const load = () => {
     let loading = document.getElementById('loading');
     let body = document.getElementsByTagName("body")[0];
-    if(cnt == 1){
         loading.style.display = `none`;
-        body.style.overflow = `scroll`;
-        clearInterval('id');
-        return;
-    }
-    cnt++;
+        body.style.overflowY = `scroll`;
 }
 
-const load = () => {
-    let id = setInterval(alertt, 2000);
+// smoothscroll.js
+const scrol = () => {
+    let scrolll = setInterval(() => {
+        if(window.pageYOffset == 0){
+            clearInterval(scrolll);
+            return;
+        }
+        else{
+            setTimeout(() => {
+                window.scrollBy(0, -20);
+            }, 100);
+        }
+    }, 20);
 }
+setTimeout(() => {
+    var move = document.querySelectorAll('.nav a')[0];
+    move.addEventListener('click', scrol);
+}, 500);
